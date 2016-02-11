510763c557c98c95181ac85dd3f405754771732e
paramaxproduction <paramaxproduction@gmail.com>
2016-02-11 18:09:17 -0500

    project start

---
+++ js/resumeBuilder.js
@@ -0,0 +1,7 @@
+var name = "Maxime St-jean";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
